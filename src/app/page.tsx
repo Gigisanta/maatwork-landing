284a309781dd47a6d712e28cc3e91e8de2b347d5
Hermes Engineer <hermes@maat.work>
2026-07-03 02:23:08 -0300

    Shorten landing story

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,7 @@ import { OperationalField } from "@/components/OperationalField";
 import { ScrollProgress } from "@/components/ScrollProgress";
 import { Navbar } from "@/components/Navbar";
 import { Hero } from "@/components/Hero";
-import { ProofStrip } from "@/components/ProofStrip";
-import { ProblemSolution } from "@/components/ProblemSolution";
 import { Services } from "@/components/Services";
-import { HowItWorks } from "@/components/HowItWorks";
 import { Pricing } from "@/components/Pricing";
 import { ProductEcosystem } from "@/components/ProductEcosystem";
 import { FinalCTA } from "@/components/FinalCTA";
@@ -20,10 +17,7 @@ export default function HomePage() {
       <Navbar />
       <main id="main-content" className="home-compact" tabIndex={-1}>
         <Hero />
-        <ProofStrip />
-        <ProblemSolution />
         <Services />
-        <HowItWorks />
         <Pricing />
         <ProductEcosystem />
         <FinalCTA />
